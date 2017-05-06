84c0d49ba6815718878b2ad99cfebb5066af709d
Nils Ohlmeier <github@ohlmeier.org>
2017-05-05 22:18:05 -0700

    moved SdpParserResult into a new error module

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,9 @@
 use std::str::FromStr;
 use std::fmt;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::num::ParseIntError;
 
-#[derive(Debug)]
-pub enum SdpParserResult {
-    ParserLineError   { message: String,
-                        line: String },
-    ParserUnsupported { message: String,
-                        line: String },
-    ParserSequence    { message: String,
-                        line: Option<usize> },
-}
-
-impl From<ParseIntError> for SdpParserResult {
-    fn from(_: ParseIntError) -> SdpParserResult {
-        // TODO empty line error here makes no sense
-        SdpParserResult::ParserLineError { message: "failed to parse integer".to_string(),
-                                           line: "".to_string() }
-    }
-}
+mod error;
+use error::SdpParserResult;
 
 #[derive(Clone)]
 pub enum SdpAttributeType {
