2ff69cdcf5cd30277c972ff4de6b4eaffe3bc64d
Nils Ohlmeier <github@ohlmeier.org>
2017-06-06 16:51:57 -0700

    rust formated

--- src/error.rs
+++ src/error.rs
@@ -2,19 +2,20 @@ use std::num::ParseIntError;
 
 #[derive(Debug)]
 pub enum SdpParserResult {
-    ParserLineError   { message: String,
-                        line: String },
-    ParserUnsupported { message: String,
-                        line: String },
-    ParserSequence    { message: String,
-                        line: Option<usize> },
+    ParserLineError { message: String, line: String },
+    ParserUnsupported { message: String, line: String },
+    ParserSequence {
+        message: String,
+        line: Option<usize>,
+    },
 }
 
 impl From<ParseIntError> for SdpParserResult {
     fn from(_: ParseIntError) -> SdpParserResult {
         // TODO empty line error here makes no sense
-        SdpParserResult::ParserLineError { message: "failed to parse integer".to_string(),
-                                           line: "".to_string() }
+        SdpParserResult::ParserLineError {
+            message: "failed to parse integer".to_string(),
+            line: "".to_string(),
+        }
     }
 }
-
