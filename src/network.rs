95b0a8f4ade1f4a61450d02448d8ebd8fcc8fc37
Nils Ohlmeier <github@ohlmeier.org>
2021-02-16 22:54:53 -0800

    moved network tests into separate file

--- src/network.rs
+++ src/network.rs
@@ -33,34 +33,5 @@ pub fn parse_unicast_address(value: &str) -> Result<Address, SdpParserInternalEr
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_network_type() -> Result<(), SdpParserInternalError> {
-        parse_network_type("iN")?;
-
-        assert!(parse_network_type("").is_err());
-        assert!(parse_network_type("FOO").is_err());
-        Ok(())
-    }
-
-    #[test]
-    fn test_parse_address_type() -> Result<(), SdpParserInternalError> {
-        let ip4 = parse_address_type("iP4")?;
-        assert_eq!(ip4, AddressType::IpV4);
-        let ip6 = parse_address_type("Ip6")?;
-        assert_eq!(ip6, AddressType::IpV6);
-
-        assert!(parse_address_type("").is_err());
-        assert!(parse_address_type("IP5").is_err());
-        Ok(())
-    }
-
-    #[test]
-    fn test_parse_unicast_address() -> Result<(), SdpParserInternalError> {
-        parse_unicast_address("127.0.0.1")?;
-        parse_unicast_address("::1")?;
-        Ok(())
-    }
-}
+#[path = "./network_tests.rs"]
+mod tests;
